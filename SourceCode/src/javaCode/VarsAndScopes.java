98ef76e17ffe08e3927b880b2287c08b74a9148d
Mark Grechanik <drmark@uic.edu>
2022-09-06 18:18:20 -0500

    sept 6

---
+++ SourceCode/src/javaCode/VarsAndScopes.java
@@ -0,0 +1,29 @@
+package javaCode;/*
+ *
+ *  Copyright (c) 2021. Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *
+ *   Unless required by applicable law or agreed to in writing, software distributed under
+ *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+public class VarsAndScopes {
+    public static int i  = 10;
+    public int j = 1000;
+    static {
+        int i = 3;
+        if( i == 3) {
+            {
+                i = 5;
+//                j = 2000;
+//                System.out.println(j);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new VarsAndScopes().i = 100;
+        System.out.println(new VarsAndScopes().i);
+    }
+}

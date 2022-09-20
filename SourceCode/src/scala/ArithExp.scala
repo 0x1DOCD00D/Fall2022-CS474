94fff2b7140553be4e4fca339cb408aa821050bb
Mark Grechanik <drmark@uic.edu>
2022-09-20 18:21:11 -0500

    sept 20

--- SourceCode/src/scala/ArithExp.scala
+++ SourceCode/src/scala/ArithExp.scala
@@ -14,7 +14,7 @@ object ArithExp:
   enum ArithmeticExpression:
     case Value(v:Int)
     case Variables(name:String)
-    case Add(o1: ArithmeticExpression, o2: ArithmeticExpression)
+    case Add(o1: ArithmeticExpression | Int, o2: ArithmeticExpression)
     case Mult(o1: ArithmeticExpression, o2: ArithmeticExpression)
     case Scope(name: String, expressions: List[ArithmeticExpression])
 
@@ -23,11 +23,14 @@ object ArithExp:
     def eval: Int = this match
       case Variables(name) => EnvironmentTable.getOrElse(name, throw new Exception(name))
       case Value(x) => x
-      case Add(o1, o2) => o1.eval + o2.eval
+      case Add(o1, o2) => o1 match
+        case v: ArithmeticExpression => v.eval
+        case v: Int => v
+        + o2.eval
       case Mult(o1, o2) => o1.eval * o2.eval
 
   @main def runArithExp =
     import ArithmeticExpression.*
 //    Scope("o1", List(Scope("o2", Scope("o3"), Add(Value(1), Value(2)))))
 //    Add(Add(Add(...)))
-    println(Add(Add(Value(1), Variables("Tam")), Add(Value(1), Value(2))))
+    println(Add(Add(1, Variables("Karan")), Add(Value(1), Value(2))).eval)
